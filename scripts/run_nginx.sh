6eccd5b4f2239a02cafe7b80887657b42b8d6f29
Jeff <jeffery.ahern@gmail.com>
2017-10-20 20:51:03 -0700

    Update run_nginx.sh

--- scripts/run_nginx.sh
+++ scripts/run_nginx.sh
@@ -3,5 +3,6 @@
 echo "################################## Run nginx"
 export DOLLAR='$'
 envsubst < ./src/configs/nginx/nginx.conf.template > /etc/nginx/nginx.conf # /etc/nginx/conf.d/default.conf
+cat /etc/nginx/nginx.conf
 nginx -g "daemon off;"
 
